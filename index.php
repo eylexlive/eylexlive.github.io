dacacedb3ba999a3a5669416d7449436793b2904
eylexlive <38813828+eylexlive@users.noreply.github.com>
2019-01-23 14:51:25 +0300

    Summary

--- index.php
+++ index.php
@@ -1,176 +1,85 @@
-<?php require_once('Connections/baglanti.php'); ?>
 <?php
-if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
+include 'header.php';
+include 'sidebar.php';
 
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+$ayarsor=$db->prepare("SELECT * FROM ayarlar");
+$ayarsor->execute();
 
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
-}
-
-mysql_select_db($database_baglanti, $baglanti);
-$query_ayar = "SELECT * FROM ayarlar WHERE id = 1";
-$ayar = mysql_query($query_ayar, $baglanti) or die(mysql_error());
-$row_ayar = mysql_fetch_assoc($ayar);
-$totalRows_ayar = mysql_num_rows($ayar);
- ob_start();?>
-<?php /*
----------------------- CNGame Minecraft Server Portalı ---------------------------------
-
-@@@@@@@@   @@@@@@@@      @@@@@@@@     @@@@@     @@@@@   @@@@@   @@@@@@@@
-@@@@@@@@   @@@@@@@@      @@@@@@@@   @@@@ @@@@   @@@@@@ @@@@@@   @@@@@@@@
-@@         @@@  @@@      @@@        @@@   @@@   @@@@ @@@ @@@@   @@@
-@@         @@@  @@@      @@@  @@@   @@@@@@@@@   @@@@  @  @@@@   @@@@@@@@
-@@         @@@  @@@      @@@   @@   @@@@@@@@@   @@@@     @@@@   @@@
-@@@@@@@@   @@@  @@@      @@@@@@@@   @@@   @@@   @@@@     @@@@   @@@@@@@@
-@@@@@@@@   @@@  @@@      @@@@@@@@   @@@   @@@   @@@@     @@@@   @@@@@@@@
-
-
-Bu Minecraft portal yazılımı CNGame tarafından oluşturulmuş ve kodlanmıştır.
-Script'in alt tarafındaki tasarım ve kodlama yazısını silmezseniz sevinirim. :)
-  ----------------------- http://www.cngame.enjin.com -----------------------
-*/?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
-<head>
-	<base href="<?php echo $row_ayar['siteurl']; ?>" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <meta name="description" content="<?php echo $row_ayar['description']; ?>">
-    <meta name="keywords" content="<?php echo $row_ayar['keywords']; ?>" />
-    <meta name="author" content="CNGame - www.cndesing.com">
-	<meta http-equiv="refresh" content="1500;URL=">
-	<title><?php echo $row_ayar['title']; ?></title>
-	<link href="css/metro.css" rel="stylesheet">
-    <link href="css/metro-responsive.css" rel="stylesheet">
-    <link href="css/metro-icons.css" rel="stylesheet">
-    <link href="css/metro-schemes.css" rel="stylesheet">
-    <link href="css/docs.css" rel="stylesheet">
-    <script src="js/jquery-2.1.3.min.js"></script>
-    <script src="js/jquery.js"></script>
-    <script src="js/metro.js"></script>
-    <script src="js/docs.js"></script>
-    <script src="js/prettify/run_prettify.js"></script>
-    <script src="js/ga.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-</head>
-
-<body>
-
-<div class="container">
-	<?php include("page/header.php"); ?>
-	
-<?php 
-// Show IF Conditional region2 
-if (@$_SESSION['kt_login_level'] == 2) {
+$ayarcek=$ayarsor->fetch(PDO::FETCH_ASSOC);
 ?>
-	
-	<?php include("page/slider.php"); ?>
-	<hr>
-<?php } 
-// endif Conditional region2
-?>     
-<?php 
-// Show IF Conditional region2 
-if (@$_SESSION['kt_login_id'] == "") {
-?>
-	
-	<?php include("page/slider.php"); ?>
-	<hr>
-<?php } 
-// endif Conditional region2
-?> 	
-	
-	
-	
-    <table width="100%" border="0">
-  <tr>
-    <td width="65%" valign="top">
-	<?php 
-// Show IF Conditional region2 
-if (@$_SESSION['kt_login_id'] == "") {
-?>
-	<?php include("page/newshome.php"); ?>
-<?php } 
-// endif Conditional region2
-?>  
+<div class="span9">
+      <div class="content">
 
+        <div class="module">
+          <div class="module-head">
+            <h3>Site Genel Ayarları</h3>
+          </div>
+          <div class="module-body">
 
-<?php 
-// Show IF Conditional region1 
-if (@$_SESSION['kt_login_id'] != "") {
-?> 
- <?php 
-// Show IF Conditional region1 
-if (@$_SESSION['kt_login_level'] == 1) {
-?>
-<?php include("musteribilgi.php"); ?>
-<?php } 
-// endif Conditional region1
-?>
- <?php 
-// Show IF Conditional region2 
-if (@$_SESSION['kt_login_level'] == 2) {
-?>
-<?php include("page/newshome.php"); ?>
- <?php } 
-// endif Conditional region2
-?>
+              <form class="form-horizontal row-fluid" action="operation.php" method="POST">
+                <div class="control-group">
+                  <label class="control-label" for="basicinput">Site Title</label>
+                  <div class="controls">
+                    <input type="text" name="site_title" value="<?php echo $ayarcek['site_title']; ?>" class="span8">
+                  </div>
+                </div>
 
-<?php } 
-// endif Conditional region1
-?> 	
-	
-	
-	</td>
-    <td width="30%" valign="top">
-	 <?php 
-// Show IF Conditional region2 
-if (@$_SESSION['kt_login_id'] == "") {
-?>
-	<?php include("page/sidebar.php"); ?>
-<?php } 
-// endif Conditional region2
-?>  
+                <div class="control-group">
+                  <label class="control-label" for="basicinput">Site Url</label>
+                  <div class="controls">
+                    <input type="text" name="site_url" value="<?php echo $ayarcek['site_url']; ?>" class="span8">
+                  </div>
+                </div>
 
-<?php 
-// Show IF Conditional region1 
-if (@$_SESSION['kt_login_id'] != "") {
-?>  
-<?php include("page/sidebaruser.php"); ?>
-<?php } 
-// endif Conditional region1
-?>   	
-	</td>
-  </tr>
-</table>
-    <?php include("page/footer.php"); ?>
-    
+                <div class="control-group">
+                  <label class="control-label" for="basicinput">Site Logo</label>
+                  <div class="controls">
+                    <input type="text" name="site_logo" value="<?php echo $ayarcek['site_logo']; ?>" class="span8">
+                  </div>
+                </div>
+
+                <div class="control-group">
+                  <label class="control-label" for="basicinput">Site Arkaplan</label>
+                  <div class="controls">
+                    <input type="text" name="site_arkaplan" value="<?php echo $ayarcek['site_arkaplan']; ?>" class="span8">
+                  </div>
+                </div>
+
+                <div class="control-group">
+                  <label class="control-label" for="basicinput">Featured Durum</label>
+                  <div class="controls">
+                    <select class="span8" name="site_featured_durum">
+                      <?php if ($ayarcek['site_featured_durum'] == 1) { ?>
+													<option value="1">Aktif</option>
+                          <option value="0">Pasif</option>
+                      <?php } else if ($ayarcek['site_featured_durum'] == 0) { ?>
+                          <option value="0">Pasif</option>
+                          <option value="1">Aktif</option>
+                      <?php } ?>
+												</select>
+                  </div>
+                </div>
+
+                <div class="control-group">
+                  <label class="control-label" for="basicinput">Featured Resim</label>
+                  <div class="controls">
+                    <input type="text" name="site_featured_resim" value="<?php echo $ayarcek['site_featured_resim']; ?>" class="span8">
+                  </div>
+                </div>
+
+                <div class="control-group">
+                  <div class="controls">
+                    <button type="submit" name="ayarsave" class="btn btn-success">Ayarları Kaydet</button>
+                  </div>
+                </div>
+
+              </form>
+          </div>
+        </div>
+      </div>
     </div>
-</body>
-</html>
-<?php ob_end_flush();?>
+
+    </div>
+    </div>
+    </div>
+<?php include 'footer.php'; ?>
--- login.php
+++ login.php
@@ -1,241 +1,73 @@
-<?php require_once('Connections/baglanti.php'); ?>
-<?php require_once('Connections/baglanti.php'); 
-// Load the common classes
-require_once('includes/common/KT_common.php');
-
-// Load the tNG classes
-require_once('includes/tng/tNG.inc.php');
-
-// Make a transaction dispatcher instance
-$tNGs = new tNG_dispatcher("");
-
-// Make unified connection variable
-$conn_baglanti = new KT_connection($baglanti, $database_baglanti);
-
-// Start trigger
-$formValidation = new tNG_FormValidation();
-$formValidation->addField("kt_login_user", true, "text", "", "", "", "");
-$formValidation->addField("kt_login_password", true, "text", "", "", "", "");
-$tNGs->prepareValidation($formValidation);
-// End trigger
-
-// Make a login transaction instance
-$loginTransaction = new tNG_login($conn_baglanti);
-$tNGs->addTransaction($loginTransaction);
-// Register triggers
-$loginTransaction->registerTrigger("STARTER", "Trigger_Default_Starter", 1, "POST", "kt_login1");
-$loginTransaction->registerTrigger("BEFORE", "Trigger_Default_FormValidation", 10, $formValidation);
-$loginTransaction->registerTrigger("END", "Trigger_Default_Redirect", 99, "{kt_login_redirect}");
-// Add columns
-$loginTransaction->addColumn("kt_login_user", "STRING_TYPE", "POST", "kt_login_user");
-$loginTransaction->addColumn("kt_login_password", "STRING_TYPE", "POST", "kt_login_password");
-// End of login transaction instance
-
-// Execute all the registered transactions
-$tNGs->executeTransactions();
-
-// Get the transaction recordset
-$rscustom = $tNGs->getRecordset("custom");
-$row_rscustom = mysql_fetch_assoc($rscustom);
-$totalRows_rscustom = mysql_num_rows($rscustom);
-
-
- ob_start();?>
-<?php
-if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
-
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
-}
-?>
-<?php
-mysql_select_db($database_baglanti, $baglanti);
-$query_ayar = "SELECT * FROM ayarlar WHERE id = 1";
-$ayar = mysql_query($query_ayar, $baglanti) or die(mysql_error());
-$row_ayar = mysql_fetch_assoc($ayar);
-$totalRows_ayar = mysql_num_rows($ayar);
- /*
----------------------- CNGame Minecraft Server Portalı ---------------------------------
-
-@@@@@@@@   @@@@@@@@      @@@@@@@@     @@@@@     @@@@@   @@@@@   @@@@@@@@
-@@@@@@@@   @@@@@@@@      @@@@@@@@   @@@@ @@@@   @@@@@@ @@@@@@   @@@@@@@@
-@@         @@@  @@@      @@@        @@@   @@@   @@@@ @@@ @@@@   @@@
-@@         @@@  @@@      @@@  @@@   @@@@@@@@@   @@@@  @  @@@@   @@@@@@@@
-@@         @@@  @@@      @@@   @@   @@@@@@@@@   @@@@     @@@@   @@@
-@@@@@@@@   @@@  @@@      @@@@@@@@   @@@   @@@   @@@@     @@@@   @@@@@@@@
-@@@@@@@@   @@@  @@@      @@@@@@@@   @@@   @@@   @@@@     @@@@   @@@@@@@@
-
-
-Bu Minecraft portal yazılımı CNGame tarafından oluşturulmuş ve kodlanmıştır.
-Script'in alt tarafındaki tasarım ve kodlama yazısını silmezseniz sevinirim. :)
-  ----------------------- http://www.cngame.enjin.com -----------------------
-*/?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+﻿<?php include "config.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<base href="<?php echo $row_ayar['siteurl']; ?>" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <meta name="description" content="<?php echo $row_ayar['description']; ?>">
-    <meta name="keywords" content="<?php echo $row_ayar['keywords']; ?>" />
-    <meta name="author" content="CNGame - www.cndesing.com">
-	<meta http-equiv="refresh" content="1500;URL=">
-	<title>Giriş Yap / <?php echo $row_ayar['title']; ?></title>
-	<link href="css/metro.css" rel="stylesheet">
-    <link href="css/metro-responsive.css" rel="stylesheet">
-    <link href="css/metro-icons.css" rel="stylesheet">
-    <link href="css/metro-schemes.css" rel="stylesheet">
-    <link href="css/docs.css" rel="stylesheet">
-    <script src="js/jquery-2.1.3.min.js"></script>
-    <script src="js/jquery.js"></script>
-    <script src="js/metro.js"></script>
-    <script src="js/docs.js"></script>
-    <script src="js/prettify/run_prettify.js"></script>
-    <script src="js/ga.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-   
-	<style>
-        .login-form {
-            width: 25rem;
-            height: 18.75rem;
-            position: fixed;
-            top: 50%;
-            margin-top: -9.375rem;
-            left: 50%;
-            margin-left: -12.5rem;
-            background-color: #ffffff;
-            opacity: 0;
-            -webkit-transform: scale(.8);
-            transform: scale(.8);
-        }
-    </style>
-
-    <script>
-
-       
-
-
-        $(function(){
-            var form = $(".login-form");
-
-            form.css({
-                opacity: 1,
-                "-webkit-transform": "scale(1)",
-                "transform": "scale(1)",
-                "-webkit-transition": ".5s",
-                "transition": ".5s"
-            });
-        });
-    </script>
-    <link href="includes/skins/mxkollection3.css" rel="stylesheet" type="text/css" media="all" />
-    <script src="includes/common/js/base.js" type="text/javascript"></script>
-    <script src="includes/common/js/utility.js" type="text/javascript"></script>
-    <script src="includes/skins/style.js" type="text/javascript"></script>
-    <?php echo $tNGs->displayValidationRules();?>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Yönetici Panel</title>
+	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link type="text/css" href="css/theme.css" rel="stylesheet">
+	<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
 </head>
-
-<body class="bg-darkTeal">
-
-<div class="login-form padding20 block-shadow">
-
-<?php 
-// Show IF Conditional region1 
-if (@$_SESSION['kt_login_id'] != "") {
-?>  
-
-<center><h1 class="text-light">404 HATA</h1></center>
-<center>
-<h3>Sayın; <?php echo $_SESSION['kt_username']; ?></h3>
-Zaten giriş yapmış bulunuyorsunuz...<br>
-Anasayfaya yönlendiriliyor<br>
-<br><br>
-<h1>
-<div data-role="preloader" data-type="metro" data-style="dark"></div>
-</h1>
-<br><br>
-<meta http-equiv="refresh" content="7;URL=index.php" />
-</center>
-
-
-<?php } 
-// endif Conditional region1
-?> 
-
-
-
-
-
-
-
- <?php 
-// Show IF Conditional region2 
-if (@$_SESSION['kt_login_id'] == "") {
-?>
-        <form method="post" id="form1" class="KT_tngformerror" action="<?php echo KT_escapeAttribute(KT_getFullUri()); ?>">
-            <h1 class="text-light">GİRİŞ YAP</h1>
-			
-            <hr class="thin"/>
-            <?php
-	echo $tNGs->getLoginMsg();
-?>
-            <br />
-            
-          <div class="input-control text full-size" data-role="input">
-                <label for="kt_login_user">Kullanıcı Adı:</label>
-                <input type="text" name="kt_login_user" id="kt_login_user" value="<?php echo KT_escapeAttribute($row_rscustom['kt_login_user']); ?>" size="32" required />
-                <button class="button helper-button clear"><span class="mif-cross"></span></button>
-            </div>
-            <br />
-            <br />
-            <div class="input-control password full-size" data-role="input">
-                <label for="kt_login_password">Parolanız:</label>
-                <input type="password" name="kt_login_password" id="kt_login_password" value="" size="32" required/>
-                <button class="button helper-button reveal"><span class="mif-looks"></span></button>
-            </div>
-            <br />
-            <div class="form-actions">
-      <button  class="button loading-cube lighten success block-shadow-success" type="submit" name="kt_login1" id="kt_login1"> GİRİŞ </button>
-                <a href="<?php echo $row_ayar['siteurl']; ?>" type="button" class="button link">Anasayfa'ya Dön</a>
-				<a href="register" type="button" class="button link">Kayıt OL</a>
-				<?php echo $tNGs->displayFieldHint("kt_login_user");?>
-				<?php echo $tNGs->displayFieldError("custom", "kt_login_user"); ?>
-				<?php echo $tNGs->displayFieldHint("kt_login_password");?>
-				<?php echo $tNGs->displayFieldError("custom", "kt_login_password"); ?>
-            </div>
-			
-        </form>
-		<?php } 
-// endif Conditional region2
-?> 
-    </div>
+<body>
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+					<i class="icon-reorder shaded"></i>
+				</a>
+			  	<a class="brand" href="login.php">
+                   Panel
+                </a>
+			</div>
+		</div>
+	</div>
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="module module-login span4 offset4">
+					<form class="form-vertical" action="operation.php" method="POST">
+						<div class="module-head">
+							<h3>Giriş Yap</h3>
+						</div>
+						<div class="module-body">
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="text" name="admin_username" placeholder="Kullanıcı Adı" required>
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="password" name="admin_password" placeholder="Şifre" required>
+								</div>
+							</div>
+						</div>
+						<div class="module-foot">
+							<div class="control-group">
+								<div class="controls clearfix">
+									<button type="submit" name="loggin" class="btn btn-primary pull-right">Giriş Yap</button>
+                                    <?php if ($_GET['status']=="no") {
+                                    echo "Bilgilerinizi kontrol edin";
+                                    } elseif ($_GET['status']=="okex") {
+                                        echo "Çıkış yaptınız.";
+                                    }
+                                    ?>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="container">
+			<b class="copyright">VonSC Script </b>
+		</div>
+	</div>
+	<script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+	<script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+	<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 </body>
-</html>
-<?php
-mysql_free_result($ayar);
- ob_end_flush();?>
