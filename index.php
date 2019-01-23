fba8b8753b1a48bbc53e3539c0b03295dffd2cd7
eylexlive <38813828+eylexlive@users.noreply.github.com>
2019-01-23 15:39:14 +0300

    Summary

--- index.php
+++ index.php
@@ -1,77 +1,224 @@
-<?php require_once 'settings.php'; ?>
-<!DOCTYPE html>
-<html lang="tr">
-	<head>
-		<meta charset="UTF-8">
-		<title><?php echo $set["site_name"]; ?></title>
-		<link rel='stylesheet' href='css/bootstrap.css'> 
-	</head>
-	<body>
-		<div class="navbar navbar-default navbar-fixed-top">
-	  <div class="container">
-	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
+<!DOCTYPE html PUBLIC>
+<html lang="tr-TR">
+<head>
 
-	      </button> <a class="navbar-brand" href="#"><?php echo $set["site_name"]; ?></a>
-
-	    </div>
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	      <!-- [Sağ Menü] -->
-	      <ul class="nav navbar-nav">
-	        <li class="active"><a href="index.php">Home</a></li>
-	      </ul>
-	      <!-- [Sol Menü] -->
-	  	<script type="text/javascript">
-			document.write(unescape('%3C%75%6C%20%63%6C%61%73%73%3D%22%6E%61%76%20%6E%61%76%62%61%72%2D%6E%61%76%20%6E%61%76%62%61%72%2D%72%69%67%68%74%22%3E%0A%20%20%20%20%20%20%20%20%3C%6C%69%3E%3C%61%20%68%72%65%66%3D%22%68%74%74%70%3A%2F%2F%62%65%72%6B%2E%70%77%22%3E%40%42%65%72%6B%50%57%3C%2F%61%3E%3C%2F%6C%69%3E%0A%20%20%20%20%20%20%3C%2F%75%6C%3E'));
-		</script>
-	    </div>
-	  </div>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	
+	<title>Vitoll Bilişim</title>
+	
+	<link rel="shortcut icon" href="dosya/resimler/vitoll-logo.png"> <!-- Burdan Sitenin İconunu Ayarla! -->
+	
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+	
+	<!-- Normal CSS -->
+	<link rel="stylesheet" href="dosya/css/ayar" />
+	
+	
+</head>
+<body>
+	
+	<div class="resim">
+	
+		<div class="container">
+			<div class="row">
+			
+				<div class="col-md-12">
+					
+					<center>
+						
+						<br /><img src="dosya/resimler/vitoll-logo.png" alt="Vitoll Bilişim - Minecraft Site Skripti" height="200" /><br /><br /> <!-- Burasını Videoda Anlattım -->
+						
+						<font size="6" face="Fantasy" color="red">------ </font><font size="6" face="Fantasy" color="#fff"> Vitoll Bilişim </font><font size="6" face="Fantasy" color="red"> ------</font> <!-- Burdan Sunucu isimini bul Ayarla -->
+						
+						<hr />
+					</center>
+					
+					
+				</div>
+			</div>
+		</div>
 	</div>
+	
+	<div class="container">
+	
+		<div class="row">
+		
+			<div class="col-md-4">
+			
+				<br /><div class="panel panel-success">
+					<div class="panel-heading"><center><font size="5" face="" color=""><b>Sunucu</b></font></center></div>
+						<div class="panel-body">
+						
+							<center>
+							
+								<font size="4" face="" color=""><b>Sunucu İsmi</b></font><br />
+								<font size="4" face="" color="">Hypixel</font><br /><br /> <!-- Burdan Sunucu Adını Ayarla -->
+								
+								<font size="4" face="" color=""><b>Oyuncu Sayısı</b></font><br />
+							
+								
+								<?php 
+							
+									require "Query.php";
+									header('Content-type: text/html; charset=utf8;');
+									$query = new Query('play.hypixel.net', 25565); #Burdan Sunucu IPsini Değiştir. Bukadar!
+									$server = @$query->get()['general'];
 
-
-	<div class="container" style="margin-top: 80px">
-	  <div class="panel panel-primary">
-	    <div class="panel-heading">
-	      <h3 class="panel-title">Top <?php echo $set["table_limit"]; ?></h3>
-	    </div>
-	    <table class="table table-striped table-hover ">
-	      <thead>
-	        <tr>
-	          <th>#</th>
-	          <th>Player</th>
-	          <th>Kills</th>
-	          <?php echo ($set["mob_kill"] == "1") ? "<th>Mob Kills</th>" : null ; ?>
-	          <th>Deaths</th>
-	        </tr>
-	      </thead>
-	      <tbody>
-			<?php 
-
-				$query = $db->query("SELECT * FROM player_stats ORDER BY kills DESC LIMIT 0,".$set["table_limit"], PDO::FETCH_ASSOC);
-				if ( $query->rowCount() ){
-			     	foreach( $query as $row ){
-						$head = ($set["head"] == "1") ? '<img src="https://minotar.net/avatar/'.$row["playername"].'/17" class="img-circle">' : null ;
-						echo "<tr>";
-						echo "<td>".$row["id"]."</td>";
-						echo "<td>".$head." ".$row["playername"]."</td>";
-						echo "<td>".$row["kills"]."</td>";
-						echo ($set["mob_kill"] == "1") ? "<td>".$row["mobkills"]."</td>" : null;
-						echo "<td>".$row["deaths"]."</td>";
-						echo "</tr>";
-			     	}
-				}
-
-			?>
-	      </tbody>
-	    </table>
-	  </div>
+									if($server['online']) {
+										echo '<font size="4" face="" color="">' . $server['numplayers'] . '</font><font size="4" face="" color="gray"> / </font><font size="4" face="" color="">' . $server['maxplayers'] . '</font>';
+										
+									} else {
+										echo '<font size="5" face="" color="red">0</font>';
+									}
+							
+								?> <br /><br />
+								
+								<font size="4" face="" color=""><b>Sunucu Sürümü</b></font><br /> 
+								<font size="4" face="" color="">1.8/1.9/1.10/1.11/1.12</font><br /><br /> <!-- Burdan Sunucu Sürümlerini Ayarla -->
+								
+								<font size="4" face="" color=""><b>Sunucu Ip</b></font><br />
+								<font size="4" face="" color="green">Play.Hypixel.Net</font> <!-- Burdan Sunucu IPsini Ayarla -->
+								
+							</center>
+						
+						</div>
+						
+				</div>
+			
+			</div>
+			
+			<div class="col-md-4">
+			
+				<br /><div class="panel panel-danger">
+					<div class="panel-heading"><center><font size="5" face="" color=""><b>Haberler</b></font></center></div>
+						<div class="panel-body"> 
+						
+							<div class="haber"> <!-- Bu Kısmı Kopyala Yapışır Yaparak Sitenize Haber Ekleyebilirsiniz! -->
+							
+								<center>
+								
+									<font size="4" ><b>Sunucumuz Yeni Açılmıştır!</b></font><br /> <!-- Başlık -->
+									
+									<font size="" face="" color=""> <!-- Buraya Yazacağınız Yazı -->
+									
+										Merhaba Arkaşlar Bu Skript İle Sitesi Olmayan <b>MineCraft Sunucularını</b> Siteli Hale Getirdik!
+									
+									</font>
+									
+									<hr />
+								
+								</center>
+							
+							</div> <!-- Buraya Kadar Kopyalanacak! -->
+							
+							<div class="haber"> <!-- Bu Kısmı Kopyala Yapışır Yaparak Sitenize Haber Ekleyebilirsiniz! -->
+							
+								<center>
+								
+									<font size="4" ><b>Vitoll Bilişim Yapımı Skript</b></font><br />
+									
+									<font size="" face="" color=""> <!-- Buraya Yazacağınız Yazı -->
+									
+										Bu Skript <b>Vitoll Bilişim</b> Tarafından Hazırlanıp <b>Sadettin Yaycı</b> Tarafından Kodlanmıştır!
+ 									
+									</font>
+									
+									<hr />
+								
+								</center>
+							
+							</div> <!-- Buraya Kadar Kopyalanacak! -->
+							
+						
+						</div>
+					</div>	
+			</div>
+			
+			<div class="col-md-4">
+			
+				<br /><div class="panel panel-info">
+					<div class="panel-heading"><center><font size="5" face="" color=""><b>Yetkililer</b></font></center></div>
+						<div class="panel-body"> 
+						
+							<table class="table table-striped table-bordered"> 
+								<thead>
+									<tr>
+									  <th class="text-center">#</th>
+									  <th>İsim</th>
+									  <th class="text-center">Yetkisi</th>
+									</tr>
+								</thead>
+								  
+								<tbody>
+									
+									<tr>
+										<td class="text-center"><img src="http://cravatar.eu/avatar/VitalYT/20.png"></td>
+										<td>VitalYT</td>
+										<td class="text-center"><b>Kurucu</b></td>
+									</tr>
+									
+									<tr>
+										<td class="text-center"><img src="http://cravatar.eu/avatar/AtakanYumru/20.png"></td>
+										<td>AtakanYumru</td>
+										<td class="text-center"><b>Kurucu</b></td>
+									</tr>
+									
+									<tr>
+										<td class="text-center"><img src="http://cravatar.eu/avatar/MrSemo/20.png"></td>
+										<td>MrSemo</td>
+										<td class="text-center"><b>Kurucu</b></td>
+									</tr>
+									
+								</tbody>
+							</table>
+						
+						</div>
+					</div>	
+			</div>
+		
+		</div>
+	
 	</div>
-
-	<script src="js/jquery-2.2.4.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-
-	</body>
+	
+	<br /><br /><br /><br /><br /><br />
+	
+	<div class="container-fluid footer-f">
+	
+		<div class="row">
+		
+			<div class="col-md-12">
+			
+				<div class="panel panel-default">
+				
+					<div class="panel-body">
+					
+						Tüm Hakları Saklıdır. Copyright © 2017 <b>Vitoll Bilişim Farkıyla!</b>
+					
+						<span class="pull-right">
+						
+							<a data-toggle="tooltip" data-placement="top" title href="http://vitoll.ml/" target="_Blank" data-orginal-title="Vitoll Bilişim Minecraft Site">
+							
+								<strong>Minecraft Site</strong>
+							
+							</a>
+							<strong> / </strong>
+							<b>Vitoll Bilişim</b>
+						</span>
+					</div>
+					
+				
+				</div>
+			
+			</div>
+		
+		</div>
+	
+	</div>
+	
+</body>
 </html>
